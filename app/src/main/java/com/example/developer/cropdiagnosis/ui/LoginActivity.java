cd95f1858a5f210f5ef72933c9a1fb5ba45f13cf
xiang_36 <xiangjianjian@hotmail.com>
2016-12-21 15:53:03 +0800

    给注册界面和登陆界面加了ToolBar

--- app/src/main/java/com/example/developer/cropdiagnosis/ui/LoginActivity.java
+++ app/src/main/java/com/example/developer/cropdiagnosis/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.developer.cropdiagnosis.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -73,6 +74,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
 
         initConfig();
--- app/src/main/java/com/example/developer/cropdiagnosis/ui/RegisterActivity.java
+++ app/src/main/java/com/example/developer/cropdiagnosis/ui/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.developer.cropdiagnosis.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -82,6 +84,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
 
         initView();
