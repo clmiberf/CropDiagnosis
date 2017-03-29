83aedac11aa0fecc3fd8390994d67dca4ebf981e
xiang_36 <xiangjianjian@hotmail.com>
2017-03-29 22:38:48 +0800

    地址选择功能搞定

--- app/src/main/java/com/example/developer/cropdiagnosis/mvp/ui/activities/EditInfoActivity.java
+++ app/src/main/java/com/example/developer/cropdiagnosis/mvp/ui/activities/EditInfoActivity.java
@@ -3,12 +3,11 @@ package com.example.developer.cropdiagnosis.mvp.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,29 +18,37 @@ import com.example.developer.cropdiagnosis.mvp.view.EditInfoView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import chihane.jdaddressselector.BottomDialog;
+import chihane.jdaddressselector.OnAddressSelectedListener;
+import chihane.jdaddressselector.model.City;
+import chihane.jdaddressselector.model.County;
+import chihane.jdaddressselector.model.Province;
+import chihane.jdaddressselector.model.Street;
 
-public class EditInfoActivity extends BaseActivity implements EditInfoView{
+public class EditInfoActivity extends BaseActivity implements EditInfoView {
 
+    @BindView(R.id.edit_info_back)
+    ImageView editInfoBack;
     @BindView(R.id.edit_info_next)
-    TextView next;
+    TextView editInfoNext;
     @BindView(R.id.edit_info_toolbar)
     Toolbar editInfoToolbar;
     @BindView(R.id.edit_info_nickname)
     EditText editInfoNickname;
     @BindView(R.id.edit_info_name)
     EditText editInfoName;
-//    @BindView(R.id.edit_info_sex)
-//    EditText editInfoSex;
     @BindView(R.id.edit_info_id)
     EditText editInfoId;
+    @BindView(R.id.edit_info_location_view)
+    ImageView editInfoLocationView;
     @BindView(R.id.edit_info_location)
     TextView editInfoLocation;
     @BindView(R.id.edit_info_arrow)
     ImageView editInfoArrow;
+    @BindView(R.id.relative_layout)
+    RelativeLayout relativeLayout;
     @BindView(R.id.edit_info_load_register)
-    ProgressBar progressBar;
-
-
+    ProgressBar editInfoLoadRegister;
 
     @Override
     public int getLayoutId() {
@@ -68,77 +75,73 @@ public class EditInfoActivity extends BaseActivity implements EditInfoView{
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_next,menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-
-    @OnClick({R.id.edit_info_toolbar, R.id.edit_info_nickname, R.id.edit_info_name, R.id.edit_info_id, R.id.edit_info_location,R.id.edit_info_next,R.id.edit_info_back})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.edit_info_toolbar:
-                break;
-            case R.id.edit_info_nickname:
-                break;
-            case R.id.edit_info_name:
-                break;
-//            case R.id.edit_info_sex:
-//                break;
-            case R.id.edit_info_id:
-                break;
-            case R.id.edit_info_location:
-                break;
-            case R.id.edit_info_next:
-                editInfo();
-                break;
-            case R.id.edit_info_back:
-                Intent it = new Intent(EditInfoActivity.this,RegisterActivity.class);
-                startActivity(it);
-                break;
-        }
-    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.next:
-//                editInfo();
-//                break;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @Override
     public void editInfoSuccess() {
         hideProgress();
-        Intent intent = new Intent(EditInfoActivity.this,HomeActivity.class);
+        Intent intent = new Intent(EditInfoActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
     }
 
     @Override
     public void editInfoFailed(String errMsg) {
-        Toast.makeText(this,errMsg,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, errMsg, Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    public void showProgress() { progressBar.setVisibility(View.VISIBLE);
+    public void showProgress() {
+        editInfoLoadRegister.setVisibility(View.VISIBLE);
 
     }
 
     @Override
-    public void hideProgress() { progressBar.setVisibility(View.GONE);
+    public void hideProgress() {
+        editInfoLoadRegister.setVisibility(View.GONE);
 
     }
 
-    private void editInfo(){
+    private void editInfo() {
         EditInfoApiImpl infoApi = new EditInfoApiImpl();
         infoApi.editInfo(editInfoNickname.getText().toString(),
                 editInfoName.getText().toString(),
                 editInfoId.getText().toString(),
-               this);
+                this);
         showProgress();
+    }
+
+    @OnClick({R.id.edit_info_back, R.id.relative_layout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edit_info_back:
+                Intent it = new Intent(EditInfoActivity.this, RegisterActivity.class);
+                startActivity(it);
+                break;
+            case R.id.relative_layout:
+                BottomDialog dialog = new BottomDialog(this);
+                dialog.setOnAddressSelectedListener(new OnAddressSelectedListener() {
+                    @Override
+                    public void onAddressSelected(Province province, City city, County county, Street street) {
+                        String location = "";
+                        if (province != null) {
+                            location += province.name;
+                        }
+                        if (city != null) {
+                            location += ' ' + city.name;
+                        }
+                        if (county != null) {
+                            location += ' ' + county.name;
+                        }
+                        if (street != null) {
+                            location += ' ' + street.name;
+                        }
+                        editInfoLocation.setText(location);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                break;
+        }
     }
 }
